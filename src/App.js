52e56489d17393d89ff337132345d53c816fde06
Axn-Switch <dietespifftimothyswift@gmail.com>
2023-02-19 15:57:38 +0000

    auth completion and route guarding

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import Profile from './pages/Profile'
 import ForgotPassword from './pages/ForgotPassword'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import { Slide, ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
+import PrivateRoute from "./components/PrivateRoute"
 
 function App() {
   return (
     <>
-      <h1>App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<PrivateRoute />}>   
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>  
+        <Navbar/>
+      </Router>
+      <ToastContainer    position="top-center" autoClose={2000} hideProgressBar={true}
+        newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss  draggable pauseOnHover
+        theme="light" transition={Slide}  />
     </>
   );
 }
