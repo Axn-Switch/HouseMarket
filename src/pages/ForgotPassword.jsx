46d9e90cd02895a4f0b238686e056f9eaf0288de
Axn-Switch <dietespifftimothyswift@gmail.com>
2023-03-04 11:29:47 +0000

    google authentication complete

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
+import { toast } from 'react-toastify'
+import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 
 function ForgotPassword() {
+  const onChange = (e) =>{
+    setEmail(e.target.value)
+  }
+  
+  const onSubmit = async(e) =>{
+    e.preventDefault()
+
+    try{
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email)
+      toast.success('Email was sent')
+    }
+    catch(error){
+      toast.error('Could not send reset email')
+    }
+  }
+  
+  const [email, setEmail] = useState('')
+
   return (
-    <div>
-        <h1>Forgot Password</h1>
+    <div className="pageConatiner">
+      <header>
+        <p className="pageHeader">Forgot Password</p>
+      </header>
+      <main>
+        <form onSubmit={onSubmit}>
+          <input type="email" className="emailInput" placeholder='email' value={email} id='email' onChange={onChange}/>
+          <Link className='forgotPasswordLink' to='/sign-in'>Sign In</Link>
+
+          <div className="signInBar">
+            <div className="signInText">Send Reset Link</div>
+            <button className="signInButton">
+              <ArrowRightIcon fill='#fff' width='34px'height='34px'/>
+            </button>
+          </div>
+        </form>
+      </main>
     </div>
   )
 }
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,6 +5,7 @@ import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRig
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import { toast } from 'react-toastify'
+import OAuth from '../components/OAuth'
 
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
@@ -54,6 +55,8 @@ function SignIn() {
               </button>
             </div>
           </form>
+          <OAuth/>
+
           <Link to='/sign-up' className='registerLink'>Sign Up Instead</Link>
         </main>
       </div>
